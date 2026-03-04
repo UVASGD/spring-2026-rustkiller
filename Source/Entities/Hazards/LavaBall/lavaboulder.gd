c9a95e88fb673fbf3767540570efac60930c888a
Aidan Liu <boomcreeper296@gmail.com>
2026-03-04 18:47:50 -0500

    Add animations and images

--- Source/Entities/Hazards/LavaBall/lavaboulder.gd
+++ Source/Entities/Hazards/LavaBall/lavaboulder.gd
@@ -1,10 +1,11 @@
 extends Node
-class_name LavaBall
+class_name LavaBoulder
 
 @onready var hitbox: HitboxComponent = $HitboxComponent
-@onready var sprite: Sprite2D = $Sprite2D
+@onready var sprite: Sprite2D = $GroundAOE
+@onready var animPlayer: AnimationPlayer = $AnimationPlayer
 var time_elapsed: float = 0
-@export var time_activate: int = 1
+@export var time_animate: int = 1
 var activated: bool = false
 @export var time_despawn: int = 3
 
@@ -17,8 +18,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	time_elapsed += delta
-	if not activated and time_elapsed >= time_activate:
-		activate()
+	if not activated and time_elapsed >= time_animate:
+		animPlayer.play("fall")
 	if time_elapsed >= time_despawn:
 		queue_free()
 	
