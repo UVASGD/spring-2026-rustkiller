a9f7955d730c2e12cec964be30474e040d0615ea
Felix-Young-Git <91382176+Felix-Young-Git@users.noreply.github.com>
2026-03-12 15:04:49 -0400

    Updated steam burst

--- Source/Entities/Projectiles/SteamBurst/steam_burst.gd
+++ Source/Entities/Projectiles/SteamBurst/steam_burst.gd
@@ -1,10 +1,23 @@
+#The boss or game manager will call to spawn the steam blasts 
+#When created, after some amount of time, the blast will trigger, 
+#and if the player is in the blast they take damage
+
 extends Area2D
+
+@export var time_until_burst : float
+@export var damage : int
+var exploding = false
 
 func _ready() -> void:
 	area_entered.connect(_on_area_entered)
+	await get_tree().create_timer(time_until_burst).timeout
+	exploding = true
 	
 	
 func _on_area_entered(other_area: Area2D) -> void:
 	if other_area is HitboxComponent and other_area.hit_owner == "player":
-		#call function to do damage on player
+		if(exploding):
+			#call function to do damage on player (only once)
+			#basically player.health -= damage
+			pass
 		pass
