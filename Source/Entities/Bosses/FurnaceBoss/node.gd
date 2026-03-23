d77ffe7047e53a46b42a77fbe5197301df15adf5
Saaswath Kumar <saaskum12@gmail.com>
2026-03-23 04:23:34 -0400

    Added phase 2 animations

--- Source/Entities/Bosses/FurnaceBoss/node.gd
+++ Source/Entities/Bosses/FurnaceBoss/node.gd
@@ -3,14 +3,17 @@ extends HFSM
 var timer := 0.0
 
 func on_enter():
-	#animator.play("idle")
-	#timer = animator.get_animation("idle_1").length
-	pass
+	if animator.has_animation("RESET"):
+		animator.play("RESET")
+		animator.advance(0.0)
+	animator.play(animation)
+	timer = animator.get_animation(animation).length
+
 func update(delta):
 	timer -= delta
 
 func check_transition(_delta) -> TransitionData:
-	if animation_ended():
+	if timer <= 0.0:
 		var alive_state := get_parent() as HFSM
 		if alive_state and alive_state.has_method("get_next_attack"):
 			return TransitionData.new(true, alive_state.get_next_attack())
