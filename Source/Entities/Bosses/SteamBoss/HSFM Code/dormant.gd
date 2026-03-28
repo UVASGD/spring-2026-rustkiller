ba49445794ec65e116f990c436b3751c9673cf95
Aidan Liu <boomcreeper296@gmail.com>
2026-03-27 22:58:36 -0400

    Replace spaces with tabs because Godot hates spaces

--- Source/Entities/Bosses/SteamBoss/HSFM Code/dormant.gd
+++ Source/Entities/Bosses/SteamBoss/HSFM Code/dormant.gd
@@ -8,52 +8,52 @@ var _player_start_pos: Vector2
 var _awaken_started := false
 
 func on_enter() -> void:
-    _awaken_started = false
-    if player:
-        _player_start_pos = player.global_position
+	_awaken_started = false
+	if player:
+		_player_start_pos = player.global_position
 
-    _set_boss_visible(false)
-    _set_boss_active(false)
-    animation = awaken_animation
+	_set_boss_visible(false)
+	_set_boss_active(false)
+	animation = awaken_animation
 
 func update(_delta: float) -> void:
-    if _awaken_started:
-        return
+	if _awaken_started:
+		return
 
-    if awaken_when_player_moves and _player_moved():
-        _awaken_started = true
-        _set_boss_visible(true)
-        _set_boss_active(false)
+	if awaken_when_player_moves and _player_moved():
+		_awaken_started = true
+		_set_boss_visible(true)
+		_set_boss_active(false)
 
 func check_transition(_delta: float) -> TransitionData:
-    # IMPORTANT: do not transition to "Alive" here.
-    # Clear dormancy and let top steam_hsfm.gd choose Alive.
-    if _awaken_started and animation_ended():
-        _set_boss_active(true)
+	# IMPORTANT: do not transition to "Alive" here.
+	# Clear dormancy and let top steam_hsfm.gd choose Alive.
+	if _awaken_started and animation_ended():
+		_set_boss_active(true)
 
-        var boss := character as HFSMSteamBoss
-        if boss:
-            boss.is_dormant = false
+		var boss := character as HFSMSteamBoss
+		if boss:
+			boss.is_dormant = false
 
-        return TransitionData.new(false, "awaken complete; top hfsm will switch to Alive")
+		return TransitionData.new(false, "awaken complete; top hfsm will switch to Alive")
 
-    return TransitionData.new(false, "")
+	return TransitionData.new(false, "")
 
 func _player_moved() -> bool:
-    if player == null:
-        return false
-    return player.global_position.distance_to(_player_start_pos) > player_move_threshold
+	if player == null:
+		return false
+	return player.global_position.distance_to(_player_start_pos) > player_move_threshold
 
 func _set_boss_visible(v: bool) -> void:
-    if character:
-        character.visible = v
+	if character:
+		character.visible = v
 
 func _set_boss_active(v: bool) -> void:
-    if character == null:
-        return
-    for n in character.get_children():
-        if n is CollisionShape2D:
-            n.disabled = not v
-        elif n is Area2D:
-            n.monitoring = v
-            n.monitorable = v
+	if character == null:
+		return
+	for n in character.get_children():
+		if n is CollisionShape2D:
+			n.disabled = not v
+		elif n is Area2D:
+			n.monitoring = v
+			n.monitorable = v
